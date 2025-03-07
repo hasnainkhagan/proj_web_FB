f9c817ece0e963ded5c663f7bfe324b1062db3d9
Hasnain ح <108486766+hasnainkhagan@users.noreply.github.com>
2025-03-07 17:47:17 +0500

    done

--- app/page.tsx
+++ app/page.tsx
@@ -176,8 +176,7 @@ export default function Home() {
        duration-200 
        hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
             >
-              Book a Call
-            </Link>
+Schedule            </Link>
             <Link
               href={whatsappLink}
               className="
